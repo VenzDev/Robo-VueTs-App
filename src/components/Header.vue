942c4ba488883af160e818102f037137513c9064
Kamil Bechta <kamil.bechta1@gmail.com>
2020-07-19 17:04:36 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <b-navbar class="navbar" type="dark" toggleable="md" fixed="top">
+    <b-container>
+      <b-link to="/" class="navbar-brand logo">Robo App</b-link>
+      <b-navbar-toggle
+        class="bg-transparent toggler"
+        target="nav-collapse"
+      ></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="mx-2" active-class="active" to="/link">
+            <span>Link</span>
+          </b-nav-item>
+          <b-nav-item active-class="active" to="/super">
+            <span>Link</span></b-nav-item
+          >
+          <b-nav-item active-class="active">
+            <span>Login</span>
+          </b-nav-item>
+          <b-nav-item active-class="active">
+            <span>Register</span>
+          </b-nav-item>
+          <div class="d-none d-md-flex">
+            <div class="topbar-divider text-white"></div>
+            <div>
+              <b-avatar class="avatar" variant="primary" text="BV"></b-avatar>
+            </div>
+          </div>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  background: linear-gradient(120deg, #7f70f5, #0ea0ff);
+  color: #fff;
+
+  & a > span {
+    position: relative;
+    overflow: hidden;
+    padding: 5px;
+
+    &::after {
+      transition: ease-in-out 0.2s;
+      content: "";
+      display: block;
+      position: absolute;
+      bottom: 0px;
+      width: 0%;
+      height: 1px;
+      background-color: white;
+    }
+    &:hover::after {
+      width: 100%;
+    }
+  }
+  & .active span::after {
+    width: 100%;
+  }
+  .login {
+    border: 1px solid white;
+    padding: 10px 15px;
+    border-radius: 23px;
+    &:hover::after {
+      width: 0;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col md="6" class="text-center">
+        <div class="homeInfo">
+          <h2>Simple Robot App Sandbox</h2>
+          <h4>Login and see what this app can do!</h4>
+        </div>
+      </b-col>
+      <b-col class="d-flex" md="6">
+        <img class="img" src="@/assets/robot.png" alt="robot" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component
+export default class Home extends Vue {}
 </script>
+
+<style scoped>
+.homeInfo {
+  background-color: #7f70f5;
+  color: white;
+  padding: 2rem;
+  border-radius: 23px;
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
+}
+.img {
+  margin: 0 auto;
+  height: 60vh;
+}
+@media (max-width: 400px) {
+  .img {
+    height: 40vh;
+  }
+}
+</style>
